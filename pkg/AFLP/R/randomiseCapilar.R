2ca9fb84a562e2997b8aabe830201f26c7ffa5d7
bosboer <bosboer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-09 16:32:45 +0000

    adding functions to randomise specimens and add the required replicates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aflp@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/AFLP/R/randomiseCapilar.R
@@ -0,0 +1,134 @@
+randomiseCapilar <- function(Specimens, Group, FirstLabID = 1, Prefix = "", nCapilar = 8, nLines = 12, QC, rReplicates = 0.1, minReplicates = 8, fillPlate = FALSE){
+	if(is.numeric(Specimens)){
+		Specimens <- factor(seq_len(Specimens))
+	} else {
+		Specimens <- factor(Specimens)
+	}
+	if(missing(Group)){
+		specList <- data.frame(Specimen = Specimens, Group = NA)
+	} else {
+		specList <- data.frame(Specimen = Specimens, Group = Group)
+	}
+	if(missing(QC)){
+		QC <- data.frame()
+	} else {
+		specList <- rbind(specList, data.frame(Specimen = QC$ID, Group = QC$Type))
+	}
+	nPlate <- ceiling(length(Specimens) / ((nCapilar * nLines - nrow(QC)) * (1 - rReplicates)))
+	Design <- expand.grid(Capilar = LETTERS[seq_len(nCapilar)], Line = seq_len(nLines), Plate = seq_len(nPlate), Specimen = NA, Replicate = NA)
+	if(nrow(QC) > 0){
+		Design <- merge(Design, QC, all.x = TRUE)
+		Design$Replicate <- with(Design, ifelse(is.na(ID), NA, paste(ID, Plate, sep = "_")))
+		Design$Specimen <- Design$ID
+		Design$ID <- NULL
+		Design$Type <- NULL
+	}
+	if(!fillPlate){
+		lastLines <- ceiling((
+			length(Specimens) + 
+			pmax(ceiling(length(Specimens) * (1 / (1 - rReplicates) - 1)), minReplicates) + 
+			nPlate * nrow(QC)
+		) / nCapilar) %% nLines
+		if(lastLines > 0){
+			toReposition <- Design[Design$Plate == nPlate & !is.na(Design$Replicate) & Design$Line > lastLines, ]
+			Design <- subset(Design, Plate < nPlate | Line <= lastLines)
+			if(nrow(toReposition) > 0){
+				toReposition$Line <- lastLines
+				while(nrow(toReposition) > 0){
+					x <- head(toReposition, 1)
+					if(is.na(Design$Replicate[with(Design, Capilar == x$Capilar & Line == x$Line & Plate == nPlate)])){
+						Design$Replicate[with(Design, Capilar == x$Capilar & Line == x$Line & Plate == nPlate)] <- x$Replicate
+						Design$Specimen[with(Design, Capilar == x$Capilar & Line == x$Line & Plate == nPlate)] <- x$Specimen
+						toReposition <- tail(toReposition, -1)
+					} else {
+						if(toReposition$Line[1] > 1){
+							toReposition$Line[1] <- toReposition$Line[1] - 1
+						} else {
+							toReposition$Line[1] <- lastLines
+							toReposition$Capilar[1] <- sample(Design$Capilar[Design$Plate == nPlate & is.na(Design$Replicate)], 1)
+						}
+					}
+				}
+			}
+		}
+	}
+	QualityControl <- subset(Design, !is.na(Replicate))
+	Design <- subset(Design, is.na(Replicate))
+	Design <- Design[with(Design, order(Plate, Line, Capilar)), ]
+	Design$Replicate<- sprintf("%s%04i", Prefix, seq_len(nrow(Design)) + FirstLabID - 1)
+	Specimens <- sample(Specimens)
+	while(sum(is.na(Design$Specimen)) - length(Specimens) >= 4){
+		Design$Prob <- 
+			(
+				table(subset(Design, is.na(Specimen))$Plate)[Design$Plate] / table(Design$Plate)[Design$Plate]
+			* 
+				table(subset(Design, is.na(Specimen))$Capilar)[Design$Capilar] / table(Design$Capilar)[Design$Capilar]
+			) ^ 10
+		#add replicate within plate and capilar
+		Remain <- subset(cast(Plate + Capilar ~ ., data = Design, subset = is.na(Specimen), value = "Prob", fun = c(length, mean)), length >= 2)
+		i <- Remain[sample(seq_len(nrow(Remain)), 1, prob = Remain$mean), 1:2]
+		lines <- sample(subset(Design, is.na(Specimen) & Plate == i$Plate & Capilar == i$Capilar)$Line, 2)
+		Design$Specimen[with(Design, Plate == i$Plate & Capilar == i$Capilar & Line %in% lines)] <- Specimens[1]
+		Design$Prob <- 
+			(
+				table(subset(Design, is.na(Specimen))$Plate)[Design$Plate] / table(Design$Plate)[Design$Plate]
+			* 
+				table(subset(Design, is.na(Specimen))$Capilar)[Design$Capilar] / table(Design$Capilar)[Design$Capilar]
+			) ^ 10
+		#add replicate within plate and between capilar
+		Remain <- subset(Design, is.na(Specimen) & Plate == i$Plate & Capilar != i$Capilar)
+		j <- Remain[sample(seq_len(nrow(Remain)), 1, prob = Remain$mean), 1:3]
+		Design$Specimen[with(Design, Plate == j$Plate & Capilar == j$Capilar & Line == j$Line)] <- Specimens[1]
+		Design$Prob <- 
+			(
+				table(subset(Design, is.na(Specimen))$Plate)[Design$Plate] / table(Design$Plate)[Design$Plate]
+			* 
+				table(subset(Design, is.na(Specimen))$Capilar)[Design$Capilar] / table(Design$Capilar)[Design$Capilar]
+			) ^ 10
+		#add replicate between plate and within capilar
+		Remain <- subset(Design, is.na(Specimen) & Plate != i$Plate & Capilar == i$Capilar)
+		j <- Remain[sample(seq_len(nrow(Remain)), 1, prob = Remain$mean), 1:3]
+		Design$Specimen[with(Design, Plate == j$Plate & Capilar == j$Capilar & Line == j$Line)] <- Specimens[1]
+		Design$Prob <- 
+			(
+				table(subset(Design, is.na(Specimen))$Plate)[Design$Plate] / table(Design$Plate)[Design$Plate]
+			* 
+				table(subset(Design, is.na(Specimen))$Capilar)[Design$Capilar] / table(Design$Capilar)[Design$Capilar]
+			) ^ 10
+		#add replicate between plate and between capilar
+		Remain <- subset(Design, is.na(Specimen) & Plate != i$Plate & Capilar != i$Capilar)
+		j <- Remain[sample(seq_len(nrow(Remain)), 1, prob = Remain$Prob), ]
+		Design$Specimen[with(Design, Plate == j$Plate & Capilar == j$Capilar & Line == j$Line)] <- Specimens[1]
+		Specimens <- Specimens[-1]
+	}
+	if(sum(is.na(Design$Specimen)) > length(Specimens)){
+		extraN <- sum(is.na(Design$Specimen)) - length(Specimens)
+		Design$Specimen[sample(which(is.na(Design$Specimen)), extraN)] <- sample(Specimens, extraN)
+	}
+	Design$Specimen[is.na(Design$Specimen)] <- sample(Specimens)
+	Design$Prob <- NULL
+	Design <- rbind(Design, QualityControl)
+	Design$Lane <- factor(Design$Line)
+	Design$Specimen <- factor(Design$Specimen)
+	Design$Replicate <- factor(Design$Replicate)
+	Design <- Design[with(Design, order(Plate, Lane, Capilar)), ]
+	if(nrow(QC) > 0){
+		return(
+			new("AFLP",
+				Specimens = specList,
+				Replicates = Design[, c("Replicate", "Specimen", "Plate", "Capilar", "Lane")],
+				QC = list(
+					Specimens = data.frame(Specimen = QC$ID, Type = QC$Type),
+					Replicates = merge(data.frame(Specimen = QC$ID, Type = QC$Type), Design)[, c("Replicate", "Type")]
+				)
+			)
+		)
+	} else {
+		return(
+			new("AFLP",
+				Specimens = specList,
+				Replicates = Design[, c("Replicate", "Specimen", "Plate", "Capilar", "Lane")]
+			)
+		)
+	}
+}
---
+++ pkg/AFLP/R/randomiseSlabgel.R
@@ -0,0 +1,134 @@
+randomiseSlabgel <- function(Specimens, Group = NULL, nReplicates = 3, nQC = 0, nLanes = 64){
+	nReplicates <- as.integer(nReplicates)
+	nQC <- as.integer(nQC)
+	nLanes <- as.integer(nLanes)
+	if(nLanes < 1 | is.na(nLanes)){
+		stop("A plate must contain at least one lane.\n")
+	}
+	if(nReplicates < 0){
+		stop("The number of replicates must be positive.\n")
+	}
+	if(nReplicates  * 3 > nLanes){
+		stop("The number of lanes must be larger than three times the number of replicates.\n")
+	}
+	if(nQC < 0){
+		stop("The number of quality control specimens must be positive.\n")
+	}
+	if(nQC + 3 * nReplicates > nLanes){
+		stop("Not enough room for the quality control specimens. Increase the number of lanes or decrease the number of replicates or quality control specimen.\n")
+	}
+	if(is.numeric(Specimens) && length(Specimens) == 1){
+		Specimens <- as.integer(Specimens)
+		if(Specimens < 1){
+			stop("The number of specimens must be at least one.\n")
+		}
+		Specimens <- seq_len(Specimens)
+	} else {
+		if(anyDuplicated(Specimens)){
+			stop("All specimen must have a unique name.\n")
+		}
+	}
+	Specimens <- factor(Specimens)
+	#create a dataframe to hold the choosen gels and randomize the specimens
+	x <- data.frame(Specimen = sample(Specimens), Plate = NA, WithinGel = NA, BetweenGel = NA)
+	#calculate the number of gels needed
+	nGels <- ceiling(nrow(x) / (nLanes - 2 * nReplicates - nQC))
+	#select the specimen for the within gel replicates
+	whichGel <- rep(seq_len(nGels), nReplicates)
+	x$Plate[seq_along(whichGel)] <- whichGel
+	x$WithinGel[seq_along(whichGel)] <- whichGel
+	#add between gel replicates for the specimens with within replicates
+	if(nGels > 1){
+		for(i in seq_along(whichGel)){
+			tabPossible <- data.frame(table(x$WithinGel[x$WithinGel != x$WithinGel[i]]))
+			tabDone <- data.frame(table(x$BetweenGel))
+			if(nrow(tabDone) > 0){
+				tabPossible <- merge(tabPossible, tabDone, all.x = TRUE, by = "Var1")
+				tabPossible$Freq.y[is.na(tabPossible$Freq.y)] <- 0
+				tabPossible$Freq <- tabPossible$Freq.x - tabPossible$Freq.y + 0.000001
+			}
+			if(nrow(tabPossible) > 1){
+				x$BetweenGel[i] <- sample(x = as.numeric(levels(tabPossible$Var1)[tabPossible$Var1]), size = 1, prob = tabPossible$Freq)
+			} else {
+				x$BetweenGel[i] <- as.numeric(levels(tabPossible$Var1))
+			}
+		}
+	} else {
+		x$BetweenGel <- x$WithinGel
+	}
+	#add the ordinairy specimens to the gels
+	whichToDo <- which(is.na(x$Plate))
+	x$Plate[whichToDo] <- rep(seq_len(nGels), ceiling(nrow(x) / nGels))[seq_along(whichToDo)]
+	#fill the gels with replicates. chooses non-replicated specimens first
+	while(nGels * (nLanes - nQC) - sum(!is.na(x[, 2:4])) > 0 & sum(is.na(x$BetweenGel)) + sum(is.na(x$WithinGel)) > 0){
+		tabPossible <- data.frame(table(na.omit(c(x$Plate, x$WithinGel, x$BetweenGel))))
+		tabPossible$Freq <- nLanes - nQC - tabPossible$Freq
+		tabPossible <- tabPossible[tabPossible$Freq > 0, ]
+		tabPossible$Var1 <- as.numeric(levels(tabPossible$Var1))[tabPossible$Var1]
+		notBetween <- is.na(x$BetweenGel) & !is.na(x$WithinGel)
+		if(sum(notBetween) > 0){
+			Candidate <- which(notBetween)
+			if(length(Candidate) > 1){
+				Candidate <- sample(Candidate, 1)
+			}
+			tabPossible <- tabPossible[tabPossible$Var1 == x$Plate[Candidate], ]
+			if(nrow(tabPossible) > 1){
+				whichGel <- sample(x = tabPossible$Var1, size = 1, prob = tabPossible$Freq)
+			} else {
+				whichGel <- x$Plate[Candidate]
+			}
+			x$BetweenGel[Candidate] <- whichGel
+		} else {
+			Replicateable <- is.na(x$WithinGel) & x$Plate %in% tabPossible$Var1
+			Candidate <- sample(which(Replicateable), 1)
+			x$WithinGel[Candidate] <- x$Plate[Candidate]
+		}
+	}
+	#extra fill of the gels if needed. Only needed when the number of specimens is smaller than half the number of lanes.
+	toFill <- nGels * (nLanes - nQC) - sum(!is.na(x[, 2:4]))
+	if(toFill > 0){
+		x <- rbind(x, data.frame(Specimen = sample(x$Specimen, toFill, replace = TRUE), Plate = sample(seq_len(nGels), toFill, replace = TRUE), WithinGel = NA, BetweenGel = NA))
+	}
+	#create a dataframe with the samples per well in the different microwell plates
+	Result <- x[, c("Specimen", "Plate")]
+	Result <- rbind(Result, na.omit(data.frame(Specimen = x$Specimen, Plate = x$WithinGel)))
+	Result <- rbind(Result, na.omit(data.frame(Specimen = x$Specimen, Plate = x$BetweenGel)))
+	#randomize the position of the samples within a plate
+	Result <- Result[order(Result$Plate, runif(nrow(Result))), ]
+	#label the positions
+	Result$Lane <- rep(seq_len(nLanes - nQC), nGels)
+	Result$Replicate <- factor(seq_len(nrow(Result)))
+	if(nQC > 0){
+		QC <- expand.grid(Specimen = seq_len(nQC), Plate = seq_len(nGels))
+		QC$Lane <- -QC$Specimen + nLanes + 1
+		QC$Replicate <- paste("QC", QC$Specimen, QC$Plate, sep = "_")
+		QC$Specimen <- paste("QC", QC$Specimen)
+		Result <- rbind(Result, QC)
+	}
+	if(!is.null(Group)){
+		Result <- merge(Result, data.frame(Specimen = Specimens, Group = Group), all.x = TRUE)
+	} else {
+		Result$Group <- NA
+	}
+	Result <- Result[order(Result$Plate, Result$Lane), c("Plate", "Lane", "Replicate", "Specimen", "Group")]
+	Result$Capilar <- factor("A")
+	if(nQC > 0){
+		return(
+			new("AFLP", 
+				Specimens = unique(Result[, c("Specimen", "Group")]),
+				Replicates = Result[, c("Replicate", "Specimen", "Plate", "Capilar", "Lane")],
+				QC = list(
+					Specimen = data.frame(Specimen = unique(QC$Specimen), Type = "QC"),
+					Replicate = data.frame(Replicate = QC$Replicate, Type = "QC")
+				)
+			)
+		)
+	} else {
+		return(
+			new("AFLP", 
+				Specimens = unique(Result[, c("Specimen", "Group")]),
+				Replicates = Result[, c("Replicate", "Specimen", "Plate", "Capilar", "Lane")]
+			)
+		)
+	}
+}
